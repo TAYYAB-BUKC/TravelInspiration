a1eecf4542056927db2d71eeafa118f267edfebb
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-10 23:58:45 +0500

    Published to Azure along with the storage account.

--- TravelInspiration.API/Program.cs
+++ TravelInspiration.API/Program.cs
@@ -1,5 +1,6 @@
 using TravelInspiration.API;
 using TravelInspiration.API.Shared.Slices;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,12 @@ builder.Services.AddHttpContextAccessor();
    
 builder.Services.RegisterApplicationServices();
 builder.Services.RegisterPersistenceServices(builder.Configuration);
+builder.Services.AddAzureClients(clientBuilder =>
+{
+	clientBuilder.AddBlobServiceClient(builder.Configuration["ConnectionStrings:TravelInspirationTableStorageConnection:blobServiceUri"]!).WithName("ConnectionStrings:TravelInspirationTableStorageConnection");
+	clientBuilder.AddQueueServiceClient(builder.Configuration["ConnectionStrings:TravelInspirationTableStorageConnection:queueServiceUri"]!).WithName("ConnectionStrings:TravelInspirationTableStorageConnection");
+	clientBuilder.AddTableServiceClient(builder.Configuration["ConnectionStrings:TravelInspirationTableStorageConnection:tableServiceUri"]!).WithName("ConnectionStrings:TravelInspirationTableStorageConnection");
+});
 
 var app = builder.Build();
 
