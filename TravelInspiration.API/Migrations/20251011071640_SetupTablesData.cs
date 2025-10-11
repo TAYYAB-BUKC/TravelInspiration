409c58f2d9eaf4048e31277338baac5505667777
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-11 12:17:59 +0500

    Migration added for setting the data in tables.

---
+++ TravelInspiration.API/Migrations/20251011071640_SetupTablesData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelInspiration.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetupTablesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Itineraries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 961, DateTimeKind.Utc).AddTicks(2977));
+
+            migrationBuilder.UpdateData(
+                table: "Itineraries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 961, DateTimeKind.Utc).AddTicks(3244));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(3332));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(4406));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(4424));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(4429));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(4432));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedOn",
+                value: new DateTime(2025, 10, 11, 7, 16, 39, 963, DateTimeKind.Utc).AddTicks(4437));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Itineraries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2576));
+
+            migrationBuilder.UpdateData(
+                table: "Itineraries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2579));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2838));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2844));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2849));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2854));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2858));
+
+            migrationBuilder.UpdateData(
+                table: "Stops",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedOn",
+                value: new DateTime(2024, 7, 4, 13, 51, 46, 455, DateTimeKind.Utc).AddTicks(2863));
+        }
+    }
+}
