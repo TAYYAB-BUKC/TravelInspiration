f277410f6929970d491f539bea93c9371f126fd1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-14 11:12:40 +0500

    - Itinerary DTO added - SQL Extension package added.

---
+++ TravelInspiration.API.Itineraries/Models/ItineraryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelInspiration.API.Itineraries.Models
+{
+	public sealed class ItineraryDto
+	{
+		public required int Id { get; set; }
+		public required string Name { get; set; }
+		public string? Description { get; set; }
+		public required string UserId { get; set; }
+	}
+}
