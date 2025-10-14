3dc17858a0b9268e5db123dc8b16c9577f42152d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-14 11:28:33 +0500

    Fetched the itineraries from the Azure SQL Database.

--- TravelInspiration.API.Itineraries/GetItinerariesFunction.cs
+++ TravelInspiration.API.Itineraries/GetItinerariesFunction.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
+using System.Data;
+using TravelInspiration.API.Itineraries.Models;
 
 namespace TravelInspiration.API.Itineraries
 {
-    public class GetItinerariesFunction
+	public class GetItinerariesFunction
 	{
         private readonly ILogger<GetItinerariesFunction> _logger;
 
@@ -15,10 +18,12 @@ namespace TravelInspiration.API.Itineraries
         }
 
         [Function("GetItinerariesFunction")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "itineraries")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "itineraries")] HttpRequest req,
+			[SqlInput(commandText: "SELECT Id, Name, Description, UserId FROM Itineraries WITH (NOLOCK)", 
+                      commandType: CommandType.Text, parameters: "", connectionStringSetting: "TravelInspirationDbConnection")] IEnumerable<ItineraryDto> itineraries)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation("C# HTTP trigger function processed a itineraries request.");
+            return new OkObjectResult(itineraries);
         }
     }
 }
