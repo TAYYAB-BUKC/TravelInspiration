fd5dc7785d071223081f608551abcccf9e5189d6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-14 12:24:27 +0500

    DbContext configured along with the entites.

---
+++ TravelInspiration.API.Itineraries/Persistence/TravelInspirationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TravelInspiration.API.Itineraries.Domain.Entities;
+
+namespace TravelInspiration.API.Itineraries.Persistence
+{
+	public sealed class TravelInspirationDbContext(
+	DbContextOptions<TravelInspirationDbContext> options) : DbContext(options)
+	{
+		public DbSet<Itinerary> Itineraries => Set<Itinerary>();
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(
+				typeof(TravelInspirationDbContext).Assembly);
+			base.OnModelCreating(modelBuilder);
+		}
+
+		public override async Task<int> SaveChangesAsync(
+			CancellationToken cancellationToken = default)
+		{
+			foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+			{
+				switch (entry.State)
+				{
+					case EntityState.Added:
+						entry.Entity.CreatedOn = DateTime.UtcNow;
+						entry.Entity.CreatedBy = "SYSTEM";
+						entry.Entity.LastModifiedOn = DateTime.UtcNow;
+						entry.Entity.LastModifiedBy = "SYSTEM";
+						break;
+					case EntityState.Modified:
+						entry.Entity.LastModifiedOn = DateTime.UtcNow;
+						entry.Entity.LastModifiedBy = "SYSTEM";
+						break;
+				}
+			}
+
+			return await base.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
--- TravelInspiration.API.Itineraries/Program.cs
+++ TravelInspiration.API.Itineraries/Program.cs
@@ -1,13 +1,22 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TravelInspiration.API.Itineraries.Persistence;
 
 var host = new HostBuilder()
 	.ConfigureFunctionsWebApplication()
-	.ConfigureServices(services =>
+	.ConfigureServices((builder, services) =>
 	{
 		services.AddApplicationInsightsTelemetryWorkerService();
 		services.ConfigureFunctionsApplicationInsights();
+
+		services.AddDbContext<TravelInspirationDbContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("TravelInspirationDbConnection"), options =>
+			{
+				options.EnableRetryOnFailure();
+			}));
 	})
 	.Build();
 
